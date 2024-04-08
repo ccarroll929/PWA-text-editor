7246518ae9c9e9760d682e6fb606d0016985060b
Christina Carroll <ccarroll0528@gmail.com>
2024-04-08 17:56:18 -0500

    implemented asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,20 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+  // Callback function that filters the requests we want to cache (in this case, JS and CSS files)
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // Plugin to cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
+      })
+    ],
+  })
+);
